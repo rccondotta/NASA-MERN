b2b93b0b3cbf2069b51b4fab1539faabc034c96a
unknown <rccondotta@hotmail.com>
2023-05-14 13:48:57 -0400

    Load planets data in tests

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -6,9 +6,12 @@ const {
     mongoDisconnect,
 } = require('../../services/mongo');
 
+const { loadPlanetsData } = require('../../models/planets.model');
+
 describe('Launches API', () => {
     beforeAll( async() => {
         await mongoConnect();
+        await loadPlanetsData();
     });
 
     afterAll( async() => {
